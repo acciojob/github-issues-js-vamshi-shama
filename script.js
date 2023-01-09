9d155f9549435587ca93758f0514696d80e30cc8
vamshi <vamshis2017@gmail.com>
2023-01-10 01:04:28 +0530

    .

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+const next = document.getElementById('load_next');
+next.addEventListener('click', getDetailsNextPage)
+
+async function getDetailsNextPage() {
+
+    const response = await fetch("https://api.github.com/repositories/1296269/issues?page=${pageNumberHere}&per_page=5");
+
+    var data = await response.json();
+    console.log(data);
+    //Show(data);
+}
